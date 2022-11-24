548c117a1f6bebc4256aa78925b5575cc7a95bf3
Alessandro Bisiani <alessandro.bisiani.1@gmail.com>
2022-11-24 17:33:54 +0100

    feat: add Dockerfile

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,18 @@
+import os
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 
+# instantiate the application
 app = Flask(__name__)
-
 api = Api(app)
 
-app.config.from_object('src.config.DevelopmentConfig')
+# set configuration
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
-
+# endpoint
 class Ping(Resource):
     def get(self):
         return {
