b70a9a5db75fabb3d9d388cd4d2ba66f83616487
Alessandro Bisiani <alessandro.bisiani.1@gmail.com>
2022-12-02 12:55:55 +0100

    style: add linter and formatters, and lint and format

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,17 @@
 import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
 # instantiate db
 db = SQLAlchemy()
+
 
 def create_app(script_info=None):
     app = Flask(__name__)
 
     # set configuration
-    app_settings = os.getenv('APP_SETTINGS')
+    app_settings = os.getenv("APP_SETTINGS")
     app.config.from_object(app_settings)
 
     # set up extension
@@ -18,13 +19,15 @@ def create_app(script_info=None):
 
     # register blueprints
     from src.api.ping import ping_blueprint
+
     app.register_blueprint(ping_blueprint)
     from src.api.users import users_blueprint
+
     app.register_blueprint(users_blueprint)
 
     # register app and db with the shell
     @app.shell_context_processor
     def ctx():
-        return {'app': app, 'db': db}
+        return {"app": app, "db": db}
 
     return app
--- src/config.py
+++ src/config.py
@@ -3,23 +3,23 @@ import os  # new
 
 class BaseConfig:
     TESTING = False
-    SECRET_KEY = 'my_precious'
+    SECRET_KEY = "my_precious"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class TestingConfig(BaseConfig):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
 
 
 class ProductionConfig(BaseConfig):
     # fix for sqlalchemy 14 not supporting postgres://
-    url = os.environ.get('DATABASE_URL')
-    if url is not None and url.startswith('postgres://'):
-        url = url.replace('postgres://', 'postgresql://', 1)
+    url = os.environ.get("DATABASE_URL")
+    if url is not None and url.startswith("postgres://"):
+        url = url.replace("postgres://", "postgresql://", 1)
 
     SQLALCHEMY_DATABASE_URI = url
